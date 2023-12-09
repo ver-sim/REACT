5cb078244a2d87ca4cb66fbcd6c2f109cc92e193
ver-sim <verdianasimonetti@gmail.com>
2023-12-09 13:21:28 +0100

    alert clock

---
+++ props/src/AlertClock.jsx
@@ -0,0 +1,4 @@
+// eslint-disable-next-line react/prop-types
+export function AlertClock({ onClick }) {
+    return <button onClick={onClick}>Click me for known the time</button>
+}
--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,10 +1,19 @@
 import { Age } from "./Age";
+import { AlertClock } from "./AlertClock";
 import { Welcome } from "./Welcome";
 
 export function App() {
+    function handleShowTime(){
+        const now = new Date;
+
+        alert(`The time is ${now.toLocaleTimeString()}`)
+    }
     return (
-        <Welcome name="John">
-            <Age age={30}/>
-        </Welcome>
+        <div>
+            <Welcome name="John">
+                <Age age={30}/>
+            </Welcome>
+            <AlertClock onClick={handleShowTime}/>
+        </div>
     );
 }
