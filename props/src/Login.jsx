454345ee92bef9132fd2f4fc18719a8ff1be94b4
ver-sim <verdianasimonetti@gmail.com>
2023-12-14 09:46:11 +0100

    add reset button

--- props/src/Login.jsx
+++ props/src/Login.jsx
@@ -26,16 +26,19 @@ export function Login({ logged }) {
         })
     }
 
-    const handelLogin = () => {
-        logged(data)
+    const handelLogin = () => logged(data);
+
+    const handleReset = () => {
+        setData(createData());
     }
-    
+
     return (
         <div>
             <input type="text" name="username" value={data.username} onChange={handelChangeInput}/>
             <input type="password" name="password" value={data.password} onChange={handelChangeInput}/>
             <input type="checkbox" name="remember" checked={data.remember} onChange={handelChangeInput}/>
             <button disabled={!data.username || !data.password} onClick={handelLogin}>Login</button>
+            <button onClick={handleReset}>Reset</button>
         </div>
     );
 }
