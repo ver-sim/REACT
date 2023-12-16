12506ef2860f602f99cf96434debc20019aaec0f
ver-sim <verdianasimonetti@gmail.com>
2023-12-16 21:37:18 +0100

    add a reset button to TodoList

--- props/src/TodoList.jsx
+++ props/src/TodoList.jsx
@@ -8,13 +8,16 @@ export const TodoList = () => {
 
     const handleTodoButton = (e) => {
         e.preventDefault()
-        setTodos([...todos, toDoAdd])
+        setTodos(()=>[...todos, toDoAdd])
+        setToDoAdd('');
     }
+
     return (
         <div>
             <form>
-                <input type="text" value={toDoAdd} onChange={(e) => setToDoAdd(e.target.value)}/>
+                <input type="text" value={toDoAdd} onChange={(e) => setToDoAdd(() => e.target.value)}/>
                 <button onClick={handleTodoButton} >Add todo</button>
+                <button type="reset" onClick={() => setTodos([])}>Reset</button>
             </form>
             <ul>
                 {todos.map((todo, index) => <li key={index}>{todo}</li>)}
