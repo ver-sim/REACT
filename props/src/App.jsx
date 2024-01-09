7ed4cd59123343e4ef8e7ab6e2d85ce4aa604052
ver-sim <verdianasimonetti@gmail.com>
2024-01-09 15:01:08 +0100

    add Link to App.jsx

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,14 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter";
 import { ShowGithubUser } from "./ShowGithubUser";
 
 export function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Welcome name="John"/>} />
-            <Route path="/counter" element={<Counter />} />
-            <Route path="users/:username" element={<ShowGithubUser />} />
-        </Routes>
+        <>
+            <Link to="/">Welcome </Link>
+            <Link to="/counter">Counter </Link>
+            <Link to="/users/:username">SwohGitHubUser </Link>
+            <Routes>  
+                <Route path="/" element={<Welcome name="John"/>} />
+                <Route path="/counter" element={<Counter />} />
+                <Route path="users/:username" element={<ShowGithubUser />} />
+            </Routes>
+        </>
     );
 }
