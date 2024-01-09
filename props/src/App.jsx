953a44d535276096a6c6d97e030c112877f80194
ver-sim <verdianasimonetti@gmail.com>
2024-01-09 14:07:54 +0100

    route show github user

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter";
+import { ShowGithubUser } from "./ShowGithubUser";
 
 export function App() {
     return (
         <Routes>
             <Route path="/" element={<Welcome name="John"/>} />
             <Route path="/counter" element={<Counter />} />
+            <Route path="users/:username" element={<ShowGithubUser />} />
         </Routes>
     );
 }
---
+++ props/src/GithubUser.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+
+export const GithubUser = ({ username }) => {
+    const [userData, setUserData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+      const fetchApi = async () => {
+        try {
+          setLoading(true);
+          const resp = await fetch(`https://api.github.com/users/${username}`);
+          if (resp.ok){
+            const json = await resp.json();
+            setUserData(json);
+            setLoading(false);
+            setError(null)
+          } else {
+            setError("there is a problem");
+            setLoading(false);
+            setUserData(null)
+          }
+  
+        } catch (error) {
+          setError(error);
+        }
+      }
+        fetchApi();
+    }, [username]);
+    console.log(username)
+    return (
+        <div>
+            {error && <h2>{error}</h2>}
+            {loading && <h2>Loading...</h2>}
+            {userData && <div><h1>User&apos;s name: {userData.name}</h1>
+              <h2>Login: {userData.login}</h2>
+              <img src={userData.avatar_url} alt="avatar" />
+            </div>}
+            
+            
+        </div>
+    );
+}
+
+// Create a GithubUser component that receives a username prop and fetches the data of the corresponding Github user from the Github API.
+// The component should render the user's name, login and avatar.
+
+//The base URL for GitHub APIs is:- https://api.github.com/
+
+// Fetching user information with its username
+// Request type: GET API endpoint: /users/{username}
