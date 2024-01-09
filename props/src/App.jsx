a473450dc7cc1b9c6e836fef44cb4f32b7a71fef
ver-sim <verdianasimonetti@gmail.com>
2024-01-09 12:13:32 +0100

    route counter

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
+import { Counter } from "./Counter";
 
 export function App() {
     return (
         <Routes>
             <Route path="/" element={<Welcome name="John"/>} />
+            <Route path="/counter" element={<Counter />} />
         </Routes>
     );
 }
---
+++ props/src/Counter.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+// eslint-disable-next-line react/prop-types
+export function Counter({ startValue = 0}) {
+    const [counter, setCounter] = useState(startValue);
+    function handleCounterClick() {
+        setCounter((count) => count + 1) 
+        // è buona pratica inserire una callback invece che un paramatro, perché via della particolare carreteristiche funzionalità di background di react
+    }
+
+    return (
+        <div>
+            <h2>How many time you&apos;ve clicked the button? {counter}</h2>
+            <button onClick={handleCounterClick}>Click me!</button>
+        </div>
+    );
+}
