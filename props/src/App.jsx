df7f040068c7c77ed460c24560ba86a17362d527
ver-sim <verdianasimonetti@gmail.com>
2024-01-10 20:18:22 +0100

    inizio

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -1,18 +1,23 @@
 import { Link, Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter";
+// import { ShowGithubUser } from "./ShowGithubUser";
+import { GithubUserList } from "./GithubUserList";
 import { ShowGithubUser } from "./ShowGithubUser";
 
 export function App() {
     return (
         <>
+            {/* <GithubUserList /> */}
             <Link to="/">Welcome </Link>
             <Link to="/counter">Counter </Link>
-            <Link to="/users/:username">SwohGitHubUser </Link>
+            <Link to="/users">SwohGitHubUser </Link>
             <Routes>  
                 <Route path="/" element={<Welcome name="John"/>} />
                 <Route path="/counter" element={<Counter />} />
-                <Route path="users/:username" element={<ShowGithubUser />} />
+                <Route path="/users" element={<GithubUserList />} >
+                    <Route path=":users" element={<ShowGithubUser />}/>
+                </Route>
                 <Route path="*" element={<p>Not found</p>} />
             </Routes>
         </>
---
+++ props/src/GithubUserList.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+// import { ShowGithubUser } from "./ShowGithubUser";
+import { Link } from "react-router-dom";
+
+export const GithubUserList = () => {
+    const [user, setUser] = useState([]);
+    useEffect(() => {
+        const fetchApi = async () => {
+            const resp = await fetch('https://api.github.com/users');
+            const json = await resp.json();
+            console.log(json);
+            setUser(json)
+        }
+        fetchApi();
+    }, []);
+
+    return (
+        <ul>
+            {user.map((val, key) => (
+                <li key={key}>
+                    <Link to="users">{val.login}</Link>
+                    
+                </li>
+            ))}
+        </ul>
+    );
+
+}
+
+// Create a GithubUserList component that fetching this API url, https://api.github.com/users, show a list of links of github usernames.
+// By clicking on a username, the ShowGithubUser component will be displayed. Add a Route to the users path that shows the GithubUserList component. In doing so, remove the /users/:username route from the App component, and add a new nested route within the /users route.
