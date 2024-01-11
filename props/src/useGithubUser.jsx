23fbf2c686d6de7693140f3445dab8c5889afe26
ver-sim <verdianasimonetti@gmail.com>
2024-01-11 21:45:45 +0100

    conditionally fetch useSWR

--- props/src/useGithubUser.jsx
+++ props/src/useGithubUser.jsx
@@ -3,7 +3,7 @@ import  useSWR  from 'swr'
 const fetchApi = (url) => fetch(url).then((resp) => resp.json())
 
 export const useGithubUser = (username) => {
-    const {data, error} = useSWR(`https://api.github.com/users/${username}`, fetchApi);
+    const {data, error} = useSWR(username ? `https://api.github.com/users/${username}` : {}, fetchApi);
 
     return {
         userData: data,
